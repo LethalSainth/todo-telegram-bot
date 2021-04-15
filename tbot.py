7dd89e04c2a48e908f565ee6ec284be6ace23457
Ik Oka4 <38618972+eye-kay@users.noreply.github.com>
2021-04-15 23:54:48 +0100

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -1,3 +1,10 @@
+"""
+Simple Bot to create a todo list and edit the list.
+
+Source: https://dev.to/lordghostx/building-a-telegram-bot-with-python-and-fauna-494i
+"""
+
+
 import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
